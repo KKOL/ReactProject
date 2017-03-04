9376b36989bfa50cbb3827e77bec5dcbc5a3f50e
KKOL <ky.clement@yahoo.fr>
2017-03-04 22:26:54 +0100

    'Camper'

---
+++ Camper_LeaderBoard/program.js
@@ -0,0 +1,118 @@
+var campersRecent = [{"username":"Manish-Giri","img":"https://avatars.githubusercontent.com/u/11348778?v=3","alltime":4245,"recent":534,"lastUpdate":"2017-03-04T15:30:14.935Z"},{"username":"sjames1958gm","img":"https://avatars.githubusercontent.com/u/4639625?v=3","alltime":5774,"recent":510,"lastUpdate":"2017-03-01T23:58:37.294Z"},{"username":"diomed","img":"https://avatars.githubusercontent.com/u/72777?v=3","alltime":3012,"recent":497,"lastUpdate":"2017-03-03T18:55:59.585Z"},{"username":"Margaret2","img":"https://avatars.githubusercontent.com/u/18198357?v=3","alltime":1532,"recent":430,"lastUpdate":"2017-03-01T23:58:40.747Z"},{"username":"Azbo400","img":"https://avatars.githubusercontent.com/u/12162628?v=3","alltime":914,"recent":397,"lastUpdate":"2017-03-01T23:58:46.005Z"},{"username":"apottr","img":"https://avatars.githubusercontent.com/u/1924021?v=3","alltime":1845,"recent":293,"lastUpdate":"2017-03-01T23:58:56.397Z"},{"username":"coymeetsworld","img":"https://avatars.githubusercontent.com/u/7891989?v=3","alltime":1939,"recent":277,"lastUpdate":"2017-03-04T15:28:59.751Z"},{"username":"Blauelf","img":"https://avatars.githubusercontent.com/u/5952026?v=3","alltime":3384,"recent":272,"lastUpdate":"2017-03-01T23:59:43.709Z"},{"username":"JLuboff","img":"https://avatars.githubusercontent.com/u/19599055?v=3","alltime":708,"recent":271,"lastUpdate":"2017-03-02T01:51:50.249Z"},{"username":"Chrono79","img":"https://avatars.githubusercontent.com/u/9571508?v=3","alltime":3253,"recent":263,"lastUpdate":"2017-03-04T15:35:31.725Z"},{"username":"revisualize","img":"https://avatars.githubusercontent.com/u/1588399?v=3","alltime":3126,"recent":261,"lastUpdate":"2017-03-04T15:32:15.433Z"},{"username":"0x0936","img":"https://avatars.githubusercontent.com/u/4724513?v=3","alltime":2349,"recent":245,"lastUpdate":"2017-03-02T01:51:27.947Z"},{"username":"Masd925","img":"https://avatars.githubusercontent.com/u/9335367?v=3","alltime":3198,"recent":231,"lastUpdate":"2017-03-04T15:31:15.151Z"},{"username":"jodylecompte","img":"https://avatars.githubusercontent.com/u/25594276?v=3","alltime":216,"recent":216,"lastUpdate":"2017-03-02T01:50:59.878Z"},{"username":"JohnnyBizzel","img":"https://avatars.githubusercontent.com/u/18394252?v=3","alltime":1094,"recent":194,"lastUpdate":"2017-03-04T15:40:04.166Z"},{"username":"khaduch","img":"https://avatars.githubusercontent.com/u/1930584?v=3","alltime":2263,"recent":175,"lastUpdate":"2017-03-04T15:31:15.151Z"},{"username":"ArielLeslie","img":"https://avatars.githubusercontent.com/u/6234475?v=3","alltime":2637,"recent":166,"lastUpdate":"2017-03-04T15:29:14.785Z"},{"username":"moigithub","img":"https://avatars.githubusercontent.com/u/7305974?v=3","alltime":2430,"recent":140,"lastUpdate":"2017-03-04T15:29:29.818Z"},{"username":"jenkynolasco11","img":"https://avatars.githubusercontent.com/u/8356035?v=3","alltime":347,"recent":135,"lastUpdate":"2017-03-04T15:47:22.917Z"},{"username":"alexanderkopke","img":"https://avatars.githubusercontent.com/u/13712290?v=3","alltime":847,"recent":118,"lastUpdate":"2017-03-04T15:31:30.208Z"},{"username":"luishendrix92","img":"https://avatars.githubusercontent.com/u/6039444?v=3","alltime":1056,"recent":115,"lastUpdate":"2017-03-04T15:29:59.900Z"},{"username":"anthonygallina1","img":"https://avatars.githubusercontent.com/u/11003055?v=3","alltime":3290,"recent":114,"lastUpdate":"2017-03-04T15:32:15.433Z"},{"username":"JohnPaulWalsh","img":"https://avatars.githubusercontent.com/u/9558634?v=3","alltime":1053,"recent":109,"lastUpdate":"2017-02-25T09:11:19.295Z"},{"username":"camperbot","img":"https://avatars.githubusercontent.com/u/13561988?v=3","alltime":3059,"recent":100,"lastUpdate":"2017-03-04T15:30:14.936Z"},{"username":"DarrenfJ","img":"https://avatars.githubusercontent.com/u/13170765?v=3","alltime":1157,"recent":94,"lastUpdate":"2017-03-04T15:32:30.515Z"},{"username":"schas002","img":"https://avatars.githubusercontent.com/u/5538659?v=3","alltime":143,"recent":91,"lastUpdate":"2017-03-01T23:57:57.017Z"},{"username":"edwin0258","img":"https://avatars.githubusercontent.com/u/12462994?v=3","alltime":309,"recent":88,"lastUpdate":"2017-03-04T15:31:15.151Z"},{"username":"GarrusNapp","img":"https://avatars.githubusercontent.com/u/20761081?v=3","alltime":442,"recent":82,"lastUpdate":"2017-02-23T00:48:32.398Z"},{"username":"TylerMoeller","img":"https://avatars.githubusercontent.com/u/12537021?v=3","alltime":1183,"recent":65,"lastUpdate":"2017-03-04T15:30:45.035Z"},{"username":"Dorcy-ndg3","img":"https://avatars.githubusercontent.com/u/21369110?v=3","alltime":295,"recent":62,"lastUpdate":"2017-02-23T01:23:52.403Z"},{"username":"mitron6","img":"https://avatars.githubusercontent.com/u/8966523?v=3","alltime":218,"recent":60,"lastUpdate":"2017-02-22T20:07:53.242Z"},{"username":"moT01","img":"https://avatars.githubusercontent.com/u/20648924?v=3","alltime":230,"recent":57,"lastUpdate":"2017-02-25T14:59:59.866Z"},{"username":"U-ways","img":"https://avatars.githubusercontent.com/u/17771168?v=3","alltime":303,"recent":56,"lastUpdate":"2016-12-04T05:42:13.355Z"},{"username":"APRim05","img":"https://avatars.githubusercontent.com/u/20326246?v=3","alltime":306,"recent":53,"lastUpdate":"2017-03-01T23:57:51.023Z"},{"username":"Adam-Lowee","img":"https://avatars.githubusercontent.com/u/18180966?v=3","alltime":51,"recent":51,"lastUpdate":"2016-04-16T21:15:00.385Z"},{"username":"ducthanh512","img":"https://avatars.githubusercontent.com/u/17810713?v=3","alltime":50,"recent":50,"lastUpdate":"2016-04-23T15:07:57.547Z"},{"username":"overmelo","img":"https://avatars.githubusercontent.com/u/17744075?v=3","alltime":348,"recent":46,"lastUpdate":"2017-03-04T15:35:01.502Z"},{"username":"silver537","img":"https://avatars.githubusercontent.com/u/19628975?v=3","alltime":572,"recent":44,"lastUpdate":"2017-01-05T19:52:40.396Z"},{"username":"arturparkhisenko","img":"https://avatars.githubusercontent.com/u/139212?v=3","alltime":53,"recent":44,"lastUpdate":"2016-07-12T01:31:21.897Z"},{"username":"3DEsprit","img":"https://avatars.githubusercontent.com/u/3156278?v=3","alltime":45,"recent":44,"lastUpdate":"2017-01-05T19:54:11.582Z"},{"username":"brusbilis","img":"https://avatars.githubusercontent.com/u/4518624?v=3","alltime":43,"recent":44,"lastUpdate":"2016-12-24T15:44:24.233Z"},{"username":"junagao","img":"https://avatars.githubusercontent.com/u/615616?v=3","alltime":45,"recent":44,"lastUpdate":"2017-03-01T23:57:40.160Z"},{"username":"nugoose","img":"https://avatars.githubusercontent.com/u/14965989?v=3","alltime":716,"recent":43,"lastUpdate":"2017-03-04T15:29:14.784Z"},{"username":"erkanderon","img":"https://avatars.githubusercontent.com/u/10497189?v=3","alltime":44,"recent":43,"lastUpdate":"2016-08-04T18:09:28.300Z"},{"username":"aj20010319","img":"https://avatars.githubusercontent.com/u/17094061?v=3","alltime":43,"recent":43,"lastUpdate":"2017-03-02T01:53:26.254Z"},{"username":"Amarillia","img":"https://avatars.githubusercontent.com/u/13259774?v=3","alltime":47,"recent":42,"lastUpdate":"2017-02-23T01:07:08.435Z"},{"username":"satishreddy04","img":"https://avatars.githubusercontent.com/u/16640809?v=3","alltime":49,"recent":41,"lastUpdate":"2016-03-22T19:42:05.690Z"},{"username":"sorinr","img":"https://avatars.githubusercontent.com/u/8637239?v=3","alltime":900,"recent":41,"lastUpdate":"2017-02-24T21:00:14.532Z"},{"username":"roerjo","img":"https://avatars.githubusercontent.com/u/14229549?v=3","alltime":54,"recent":40,"lastUpdate":"2017-03-02T00:03:53.595Z"},{"username":"Cyclokitty","img":"https://avatars.githubusercontent.com/u/14623520?v=3","alltime":488,"recent":38,"lastUpdate":"2017-03-04T15:31:00.101Z"},{"username":"maz-net-au","img":"https://avatars.githubusercontent.com/u/9792899?v=3","alltime":1530,"recent":38,"lastUpdate":"2017-03-04T15:35:01.502Z"},{"username":"Marmiz","img":"https://avatars.githubusercontent.com/u/16669241?v=3","alltime":355,"recent":38,"lastUpdate":"2017-02-22T19:57:28.065Z"},{"username":"TamerTawfik","img":"https://avatars.githubusercontent.com/u/17963361?v=3","alltime":45,"recent":37,"lastUpdate":"2016-12-24T12:47:56.955Z"},{"username":"Cesco9525","img":"https://avatars.githubusercontent.com/u/18629215?v=3","alltime":1,"recent":36,"lastUpdate":"2017-01-05T20:09:09.533Z"},{"username":"TheOnlyTaylor","img":"http://i.imgur.com/d1jj5Ac.jpg?4","alltime":58,"recent":36,"lastUpdate":"2017-02-22T20:30:36.793Z"},{"username":"friscomm","img":"https://avatars.githubusercontent.com/u/13112533?v=3","alltime":43,"recent":35,"lastUpdate":"2016-07-12T00:08:07.277Z"},{"username":"jbcharrier","img":"https://avatars.githubusercontent.com/u/16373398?v=3","alltime":34,"recent":33,"lastUpdate":"2016-07-12T05:05:43.632Z"},{"username":"leatheronly","img":"https://avatars.githubusercontent.com/u/11307309?v=3","alltime":48,"recent":33,"lastUpdate":"2016-12-24T17:57:43.314Z"},{"username":"haidzejer","img":"https://avatars.githubusercontent.com/u/16363902?v=3","alltime":34,"recent":33,"lastUpdate":"2016-12-24T23:25:21.555Z"},{"username":"PhoenixFireVB","img":"https://avatars.githubusercontent.com/u/22198813?v=3","alltime":34,"recent":33,"lastUpdate":"2017-03-02T00:03:37.065Z"},{"username":"wordyallen","img":"https://avatars.githubusercontent.com/u/4591833?v=3","alltime":214,"recent":32,"lastUpdate":"2017-03-04T15:32:00.346Z"},{"username":"mohannadsawalhi","img":"https://avatars.githubusercontent.com/u/17391242?v=3","alltime":32,"recent":32,"lastUpdate":"2016-03-08T17:44:15.855Z"},{"username":"JohnTendik","img":"https://avatars.githubusercontent.com/u/4714913?v=3","alltime":171,"recent":31,"lastUpdate":"2016-08-08T10:34:51.778Z"},{"username":"personalspace","img":"https://avatars.githubusercontent.com/u/7895554?v=3","alltime":31,"recent":31,"lastUpdate":"2017-03-02T00:03:35.149Z"},{"username":"rahsheen","img":"https://avatars.githubusercontent.com/u/4641959?v=3","alltime":30,"recent":29,"lastUpdate":"2017-03-04T15:43:51.110Z"},{"username":"Rafase282","img":"https://avatars.githubusercontent.com/u/285138?v=3","alltime":1105,"recent":28,"lastUpdate":"2017-03-04T15:29:59.899Z"},{"username":"skyevil12","img":"https://avatars.githubusercontent.com/u/6906187?v=3","alltime":29,"recent":28,"lastUpdate":"2017-01-05T19:57:59.716Z"},{"username":"adityaparab","img":"https://avatars.githubusercontent.com/u/5561498?v=3","alltime":413,"recent":27,"lastUpdate":"2016-12-04T03:37:40.891Z"},{"username":"burhanuddin353","img":"https://avatars.githubusercontent.com/u/3624363?v=3","alltime":27,"recent":27,"lastUpdate":"2017-03-02T00:03:28.969Z"},{"username":"storbeck","img":"https://avatars.githubusercontent.com/u/449874?v=3","alltime":1495,"recent":26,"lastUpdate":"2017-03-04T15:32:15.433Z"},{"username":"CodeHunter24","img":"https://avatars.githubusercontent.com/u/12293368?v=3","alltime":28,"recent":26,"lastUpdate":"2016-08-04T18:06:26.101Z"},{"username":"texas2010","img":"https://avatars.githubusercontent.com/u/9701609?v=3","alltime":374,"recent":26,"lastUpdate":"2016-10-26T20:46:48.627Z"},{"username":"jdraiv","img":"https://avatars.githubusercontent.com/u/17892416?v=3","alltime":95,"recent":26,"lastUpdate":"2017-02-12T00:23:48.688Z"},{"username":"qualitymanifest","img":"https://avatars.githubusercontent.com/u/13897419?v=3","alltime":1184,"recent":25,"lastUpdate":"2017-03-04T15:30:29.982Z"},{"username":"alejandrobabio","img":"https://avatars.githubusercontent.com/u/1581649?v=3","alltime":26,"recent":25,"lastUpdate":"2016-09-29T16:26:44.481Z"},{"username":"RGero215","img":"https://avatars.githubusercontent.com/u/10387470?v=3","alltime":34,"recent":25,"lastUpdate":"2016-07-11T20:33:32.455Z"},{"username":"pahosler","img":"https://avatars.githubusercontent.com/u/11909710?v=3","alltime":65,"recent":24,"lastUpdate":"2016-09-08T20:47:41.369Z"},{"username":"dpberry552","img":"https://avatars.githubusercontent.com/u/19177632?v=3","alltime":25,"recent":24,"lastUpdate":"2016-12-24T23:02:18.499Z"},{"username":"dubster-808","img":"https://avatars.githubusercontent.com/u/20782073?v=3","alltime":24,"recent":24,"lastUpdate":"2017-02-23T00:49:18.961Z"},{"username":"baileypa","img":"https://avatars0.githubusercontent.com/u/9310749?v=3","alltime":24,"recent":23,"lastUpdate":"2017-03-04T15:42:50.591Z"},{"username":"erictleung","img":"https://avatars.githubusercontent.com/u/2754821?v=3","alltime":94,"recent":23,"lastUpdate":"2016-07-12T01:20:20.258Z"},{"username":"jemagee","img":"https://avatars.githubusercontent.com/u/3190083?v=3","alltime":42,"recent":23,"lastUpdate":"2016-09-08T20:52:59.842Z"},{"username":"forestOfAbstraction","img":"https://avatars.githubusercontent.com/u/23236988?v=3","alltime":23,"recent":23,"lastUpdate":"2016-12-24T23:25:52.627Z"},{"username":"paperplanegirl","img":"https://avatars.githubusercontent.com/u/20817485?v=3","alltime":24,"recent":23,"lastUpdate":"2017-02-25T14:38:28.018Z"},{"username":"rhhhhl","img":"https://avatars.githubusercontent.com/u/12888794?v=3","alltime":22,"recent":22,"lastUpdate":"2016-08-03T20:51:13.145Z"},{"username":"PhilJayN","img":"https://avatars.githubusercontent.com/u/13914700?v=3","alltime":39,"recent":22,"lastUpdate":"2016-12-01T21:52:13.247Z"},{"username":"Jonnokc","img":"https://avatars.githubusercontent.com/u/16293609?v=3","alltime":23,"recent":22,"lastUpdate":"2016-09-19T19:06:52.652Z"},{"username":"walidashri","img":"https://avatars.githubusercontent.com/u/19739328?v=3","alltime":564,"recent":22,"lastUpdate":"2017-02-23T01:11:45.637Z"},{"username":"tsalexey544","img":"https://avatars.githubusercontent.com/u/13120976?v=3","alltime":29,"recent":22,"lastUpdate":"2017-02-25T14:55:08.245Z"},{"username":"dodwellwebdev","img":"https://avatars.githubusercontent.com/u/20287039?v=3","alltime":23,"recent":22,"lastUpdate":"2017-02-25T15:15:22.687Z"},{"username":"rpemberton","img":"https://avatars.githubusercontent.com/u/19560029?v=3","alltime":22,"recent":22,"lastUpdate":"2017-02-25T15:18:42.474Z"},{"username":"vinaypuppal","img":"https://avatars.githubusercontent.com/u/8843216?v=3","alltime":354,"recent":21,"lastUpdate":"2017-03-04T15:37:02.467Z"},{"username":"Shwrndasink","img":"https://avatars.githubusercontent.com/u/5263367?v=3","alltime":37,"recent":21,"lastUpdate":"2016-08-03T21:08:19.138Z"},{"username":"Antony-Ndungu","img":"https://avatars.githubusercontent.com/u/17404185?v=3","alltime":36,"recent":21,"lastUpdate":"2016-12-03T22:19:17.092Z"},{"username":"jimmiehansson","img":"https://avatars.githubusercontent.com/u/5203605?v=3","alltime":38,"recent":21,"lastUpdate":"2016-11-11T18:49:16.145Z"},{"username":"Baskalo","img":"https://avatars.githubusercontent.com/u/20235220?v=3","alltime":32,"recent":21,"lastUpdate":"2017-02-25T13:17:59.863Z"},{"username":"timgrossmann","img":"https://avatars.githubusercontent.com/u/16529337?v=3","alltime":55,"recent":20,"lastUpdate":"2016-07-12T06:29:47.571Z"},{"username":"DanielNora","img":"https://avatars.githubusercontent.com/u/1857065?v=3","alltime":46,"recent":20,"lastUpdate":"2016-12-03T23:18:27.970Z"},{"username":"IMBurbank","img":"https://avatars.githubusercontent.com/u/21133719?v=3","alltime":47,"recent":20,"lastUpdate":"2017-03-02T00:03:43.047Z"},{"username":"eeflores","img":"https://avatars.githubusercontent.com/u/4065110?v=3","alltime":636,"recent":19,"lastUpdate":"2017-03-04T15:31:30.208Z"}];
+
+// sorting the campers list in ascending order
+campersRecent = campersRecent.sort((first, second)=>first.recent-second.recent).reverse();
+
+
+/******************************************************
+          REACT CODE
+
+*******************************************************/
+class CampersImage extends React.Component{
+  render(){
+    return <img class="campersImage" src={this.props.image_url} alt="No Image"/>
+  }
+}
+
+/*******************************************************/
+class CampersUserName extends React.Component{
+  render(){
+    return(
+      <div class= "campersUserName">{this.props.name}</div>
+    )
+  }
+}
+
+/*******************************************************/
+
+
+/*******************************************************/
+class CampersList extends React.Component{
+
+   constructor(props){
+      super(props);
+      this.state = {
+        data:campersRecent,
+        recent:true,
+        alltime:false,
+      };
+     this.handleChange = this.handleChange.bind(this);
+     this.recentClick = this.recentClick.bind(this);
+     this.alltimeClick = this.alltimeClick.bind(this);
+
+  }
+
+   handleChange(e){
+
+     this.setState(prevState=>({
+       data:prevState.data.reverse()
+     }))
+   }
+  recentClick(){
+      if(!this.state.recent){
+        this.setState(prevState=>({
+          recent:!prevState.recent,
+          data:prevState.data.reverse(),
+          alltime:false,
+        }))
+      }
+  }
+
+  alltimeClick(){
+      if(!this.state.alltime){
+        this.setState(prevState=>({
+          recent:!prevState.recent,
+          data:prevState.data.reverse(),
+          alltime:true,
+        }))
+      }
+   }
+
+    render(){
+         const List = this.state.data.map(function(camper, index){
+                return (
+                    <tr className={index%2===0?'paire':'impaire'}>
+                    <th className="index">{(index+1).toString()}</th>
+                     <th className="item">
+                       <CampersImage image_url={camper.img}/>
+                       <CampersUserName name={camper.username}/>
+                        </th>
+                     <th className="item">{camper.recent}</th>
+                     <th className="item">{camper.alltime}</th>
+                   </tr>
+
+                )
+   });
+
+        return (
+          <table>
+            <tr>
+              <th className="index"> Rang </th>
+              <th className="item"> Camper Name</th>
+              <th className="item" onClick = {this.recentClick}> Points in 30 last Days </th>
+              <th className="item" onClick = {this.alltimeClick}> All time Point </th>
+              </tr>
+
+            {List}
+            </table>
+        )
+    }
+}
+
+/*******************************************************/
+class BoardTable extends React.Component{
+    render(){
+        return (
+          <div className="boardTable">
+            <h2> LeaderBoard</h2>
+            <CampersList/>
+          </div>
+        )
+    }
+}
+
+/*******************************************************/
+ReactDOM.render(
+    <BoardTable/>,
+    document.getElementById("campers_list")
+)
